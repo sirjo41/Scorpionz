55eddaa19ef3c6ff0c1f10e190aa2122f6cfc106
siraj <srjbadri2008@gmail.com>
2025-09-22 13:19:30 +0200

    Styling 0.42

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,9 @@ extensions = [
     "sphinx_design",
 ]
 
+pygments_style = "friendly"
+pygments_dark_style = "native"
+
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
@@ -28,6 +31,14 @@ html_logo = "_static/logo.jpg"
 html_theme = "furo"
 html_theme_options = {
     "sidebar_hide_name": True,
+    "light_css_variables": {
+        "color-brand-primary": "#6a5acd",    # brand
+        "color-brand-content": "#2563eb",    # links
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#8b7bff",
+        "color-brand-content": "#93c5fd",
+    },
 }
 html_static_path = ['_static']
 
